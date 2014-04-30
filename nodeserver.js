448de529007dfc6ba95266d4f68cd052e5110273
oscargonzalez <oscar.gonzalez.varela@gmail.com>
2014-04-30 22:47:29 +0200

    Añadido socket.io Añadida comunicación Arduino - Node - Web Prueba de transmisión de datos con span

--- nodeserver.js
+++ nodeserver.js
@@ -4,13 +4,19 @@
 	www.bricogeek.com
 */
 
-var  SYSTEM_NAME 	= "Ambrosio Node Webserver";
-var  HTTP_PORT 		= 8080;	// Webserver port
+var  SYSTEM_NAME 	  = "Ambrosio Node Webserver";
+var  HTTP_PORT 		  = 8080;	// Webserver port
+var  ARDUINO_PORT   = 5000; // Arduino
+var  WEBSOCKET_PORT = 5001; // Web (websockets)
+var  CONTROL_START  = '1';
+var  CONTROL_PAUSE  = '2';
+var  CONTROL_STOP   = '3';
 
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , express = require('express');
+  , express = require('express')
+  , net = require('net');
 
 server.listen(HTTP_PORT);
 console.log(SYSTEM_NAME+' listening on port '+HTTP_PORT);
@@ -24,71 +30,96 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+var clients = [];
+var socketWeb;
+
+// Eventos de la web
+app.get('/start', function (req, res) {
+  //res.sendfile(__dirname + '/app.css');  
+  console.log('Start!');
+  clients[0].write(CONTROL_START);
+});
+
+app.get('/pause', function (req, res) {
+  //res.sendfile(__dirname + '/app.css');  
+  console.log('Pause!');
+  clients[0].write(CONTROL_PAUSE);
+});
+
+app.get('/stop', function (req, res) {  
+  console.log('Stop!');
+  clients[0].write(CONTROL_STOP);
+});  
 
 
-// usernames which are currently connected to the chat
-var usernames = {};
+// ---------------------
+
+/*
+  Recibir:
+    4 rojo
+    5 azul
+    6 blanco
+
+  Enviar a Arduino:
+    1: Start
+    2: Pausa
+    3: Stop
+*/
+
+// Conexión de Arduino
+net.createServer(function (socket) {  
+ 
+  // Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+
+  // Put this new client in the list
+  clients.push(socket);   
+ 
+  console.log("Conectado!");
+  socket.write("Hola Arduino!\n");
+
+  // Incoming data from client
+  socket.on('data', function (data) {
+    console.log("data: "+data);
+    socketWeb.emit('arduino', { color: data });
+  });  
+
+  // Cliente desconectado
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    console.log("Arduino desconectado!");
+  });  
+
+  
+}).listen(ARDUINO_PORT);
+
+// ----------
+
+var app2 = require('http').createServer(handler)
+  , io = require('socket.io').listen(app2)
+  , fs = require('fs')
+
+app2.listen(WEBSOCKET_PORT);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 io.sockets.on('connection', function (socket) {
-
-// when the client emits 'sendchat', this listens and executes
-socket.on('sendchat', function (data) {
-// we tell the client to execute 'updatechat' with 2 parameters
-io.sockets.emit('updatechat', socket.username, data);
+  socketWeb = socket;
+  socket.emit('arduino', { color: 'rojo' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
-
-// when the client emits 'opencmd', this listens and executes
-socket.on('opencmd', function (data) {
-// Add calls to IO here
-io.sockets.emit('opencmd', socket.username, data);
-setTimeout(function () {
-  io.sockets.emit('openvalve', socket.username, data);
-}, 1000)
-});
-
-// when the client emits 'closecmd', this listens and executes
-socket.on('closecmd', function (data) {
-// Add calls to IO here
-io.sockets.emit('closecmd', socket.username, data);
-setTimeout(function () {
-  io.sockets.emit('closevalve', socket.username, data);
-}, 1000)
-});
-
-// when the client emits 'adduser', this listens and executes
-socket.on('adduser', function(username){
-// we store the username in the socket session for this client
-socket.username = username;
-// add the client's username to the global list
-usernames[username] = username;
-// echo to client they've connected
-socket.emit('updatechat', 'SERVER', 'you have connected');
-// echo globally (all clients) that a person has connected
-socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-// update the list of users in chat, client-side
-io.sockets.emit('updateusers', usernames);
-});
-
-// when the user disconnects.. perform this
-socket.on('disconnect', function(){
-// remove the username from global usernames list
-delete usernames[socket.username];
-// update list of users in chat, client-side
-io.sockets.emit('updateusers', usernames);
-// echo globally that this client has left
-socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-});
-
-});
-
-
-/*var net = require('net');
-
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
-});
-
-server.listen(1337, '127.0.0.1');
-console.log('Ambrosio Node Server started on port 1337\n');
-*/
